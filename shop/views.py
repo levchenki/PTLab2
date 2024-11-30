18c58c63618cb5676f5775900b5ff518eba7ff92
levchenki <danila.levchenki@gmail.com>
2024-12-01 00:26:39 +0300

    solution for variant 9

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
+import datetime
+
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
@@ -15,7 +18,21 @@ class PurchaseCreate(CreateView):
     model = Purchase
     fields = ['product', 'person', 'address']
 
-    def form_valid(self, form):
-        self.object = form.save()
-        return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['birthdate'] = forms.DateField(
+            required=True,
+            widget=forms.DateInput(attrs={'type': 'date'}),
+            label='Дата рождения'
+        )
+        return form
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+
+        birthdate = form.cleaned_data.get('birthdate')
+        self.object.final_cost = self.object.calculate_final_cost(birthdate)
+        self.object.save()
+
+        return HttpResponse(
+            f'Спасибо за покупку, {self.object.person}! Итоговая стоимость: {self.object.final_cost} рублей.')
